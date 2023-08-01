6109cb98bf60a280b33e484c5c4be35228b6504f
Zayed Fahim <zayedfahim33.cse@gmail.com>
2023-08-01 13:32:27 +0600

    teacher event manager done

--- src/Pages/Dashboard/Home/Teacher/NoticeBoard/AddEventForStudent.js
+++ src/Pages/Dashboard/Home/Teacher/NoticeBoard/AddEventForStudent.js
@@ -1,142 +1,203 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../../../../Contexts/AuthProvider/AuthProvider";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const AddEventForStudent = () => {
+  const { teacher } = useContext(AuthContext);
   const [eventType, setEventType] = useState("");
-  const [classSelection, setClassSelection] = useState("");
+  const [assignedClass, setAssignedClass] = useState("");
   const [group, setGroup] = useState("");
   const [shift, setShift] = useState("");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [image, setImage] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Handle form submission logic here
+    const eventDetails = {
+      eventCreator: {
+        id: teacher?._id,
+        name: teacher?.fullName,
+        schoolTag: teacher?.schoolTag,
+      },
+      eventType,
+      assignedClass,
+      shift,
+      group,
+      title,
+      description,
+      startDate,
+      endDate,
+    };
+    try {
+      await axios
+        .post(
+          "https://v1-amader-school-server.vercel.app/api/v1/events",
+          eventDetails
+        )
+        .then((data) => {
+          if (data.status === 200) {
+            window.location.reload();
+            setTimeout(() => {
+              toast.success("Event Added!!");
+            });
+          }
+        });
+    } catch (error) {
+      if (error) {
+        toast.error("Attempt Unsuccessful.Please Try again.");
+      }
+    }
   };
 
   const handleReset = () => {
     setEventType("");
-    setClassSelection("");
+    setAssignedClass("");
     setGroup("");
     setShift("");
     setTitle("");
     setDescription("");
-    setImage("");
+    setStartDate("");
+    setEndDate("");
   };
 
   return (
-    <div className="bg-white 2xl:px-8 2xl:py-10 w-[900px]">
+    <div className="bg-white 2xl:px-8 2xl:pb-14 2xl:pt-10 w-[1000px] h-full">
       <h1 className="text-2xl font-bold mb-4">Add Event</h1>
       <form onSubmit={handleSubmit} className="mx-auto">
-        <div className="mb-4">
-          <label htmlFor="eventType" className="block font-bold mb-2">
-            Event Type
-          </label>
-          <select
-            id="eventType"
-            className="w-full border border-gray-300 p-2 rounded focus:outline-none"
-            value={eventType}
-            onChange={(e) => setEventType(e.target.value)}
-          >
-            <option value="">Select Event Type</option>
-            <option value="assignment">Assignment</option>
-            <option value="quiz">Quiz</option>
-            <option value="classTest">Class Test</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="title" className="block font-bold mb-2">
-            Title
-          </label>
-          <input
-            type="text"
-            id="title"
-            className="w-full border border-gray-300 p-2 rounded focus:outline-none"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="description" className="block font-bold mb-2">
-            Description
-          </label>
-          <textarea
-            id="description"
-            className="w-full border border-gray-300 p-2 rounded focus:outline-none"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="classSelection" className="block font-bold mb-2">
-            Class
-          </label>
-          <select
-            id="classSelection"
-            className="w-full border border-gray-300 p-2 rounded focus:outline-none"
-            value={classSelection}
-            onChange={(e) => setClassSelection(e.target.value)}
-          >
-            <option value="">Select Class</option>
-            <option value="play">Play</option>
-            <option value="kg">KG</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="group" className="block font-bold mb-2">
-            Group
-          </label>
-          <select
-            id="group"
-            className="w-full border border-gray-300 p-2 rounded focus:outline-none"
-            value={group}
-            onChange={(e) => setGroup(e.target.value)}
-          >
-            <option value="">Select Group</option>
-            <option value="science">Science</option>
-            <option value="arts">Arts</option>
-            <option value="commerce">Commerce</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="shift" className="block font-bold mb-2">
-            Shift
-          </label>
-          <select
-            id="shift"
-            className="w-full border border-gray-300 p-2 rounded focus:outline-none"
-            value={shift}
-            onChange={(e) => setShift(e.target.value)}
-          >
-            <option value="">Select Shift</option>
-            <option value="morning">Morning</option>
-            <option value="day">Day</option>
-          </select>
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="image" className="block font-bold mb-2">
-            Image (Optional)
-          </label>
-          <input
-            type="file"
-            id="image"
-            className="w-full border border-gray-300 p-2 rounded focus:outline-none"
-            value={image}
-            onChange={(e) => setImage(e.target.value)}
-          />
+        <div className="grid grid-cols-2 gap-5">
+          <div className="mb-4">
+            <label htmlFor="eventType" className="block font-bold mb-2">
+              Event Type
+            </label>
+            <select
+              id="eventType"
+              className="w-full border border-gray-300 p-2 rounded focus:outline-none"
+              value={eventType}
+              onChange={(e) => setEventType(e.target.value)}
+            >
+              <option value="">Select Event Type</option>
+              <option value="Assignment">Assignment</option>
+              <option value="Quiz">Quiz</option>
+              <option value="Class Test">Class Test</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="title" className="block font-bold mb-2">
+              Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              className="w-full border border-gray-300 p-2 rounded focus:outline-none"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="description" className="block font-bold mb-2">
+              Description
+            </label>
+            <textarea
+              id="description"
+              className="w-full border border-gray-300 p-2 rounded focus:outline-none"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="assignedClass" className="block font-bold mb-2">
+              Class
+            </label>
+            <select
+              id="assignedClass"
+              className="w-full border border-gray-300 p-2 rounded focus:outline-none"
+              value={assignedClass}
+              onChange={(e) => setAssignedClass(e.target.value)}
+            >
+              <option value="">Select Class</option>
+              <option value="play">Play</option>
+              <option value="kg">KG</option>
+              <option value="One">One</option>
+              <option value="Two">Two</option>
+              <option value="Three">Three</option>
+              <option value="Four">Four</option>
+              <option value="Five">Five</option>
+              <option value="Six">Six</option>
+              <option value="Seven">Seven</option>
+              <option value="Eight">Eight</option>
+              <option value="Nine">Nine</option>
+              <option value="Ten">Ten</option>
+              <option value="Eleven">Eleven</option>
+              <option value="Twelve">Twelve</option>
+            </select>
+          </div>
+          {assignedClass &&
+            (assignedClass === "Nine" ||
+              assignedClass === "Ten" ||
+              assignedClass === "Eleven" ||
+              assignedClass === "Twelve") && (
+              <div className="mb-4">
+                <label htmlFor="group" className="block font-bold mb-2">
+                  Group
+                </label>
+                <select
+                  id="group"
+                  className="w-full border border-gray-300 p-2 rounded focus:outline-none"
+                  value={group}
+                  onChange={(e) => setGroup(e.target.value)}
+                >
+                  <option value="">Select Group</option>
+                  <option value="Science">Science</option>
+                  <option value="Arts">Arts</option>
+                  <option value="Commerce">Commerce</option>
+                </select>
+              </div>
+            )}
+          <div className="mb-4">
+            <label htmlFor="shift" className="block font-bold mb-2">
+              Shift
+            </label>
+            <select
+              id="shift"
+              className="w-full border border-gray-300 p-2 rounded focus:outline-none"
+              value={shift}
+              onChange={(e) => setShift(e.target.value)}
+            >
+              <option value="">Select Shift</option>
+              <option value="Morning">Morning</option>
+              <option value="Day">Day</option>
+            </select>
+          </div>
+          {eventType && eventType === "Assignment" && (
+            <div className="mb-4">
+              <label htmlFor="startDate" className="block font-bold mb-2">
+                Start Date
+              </label>
+              <input
+                type="date"
+                id="startDate"
+                className="w-full border border-gray-300 p-2 rounded focus:outline-none"
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
+            </div>
+          )}
+          <div className="mb-4">
+            <label htmlFor="endDate" className="block font-bold mb-2">
+              {eventType === "Assignment" ? <p>End Date</p> : <p>Date</p>}
+            </label>
+            <input
+              type="date"
+              id="endDate"
+              className="w-full border border-gray-300 p-2 rounded focus:outline-none"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </div>
         </div>
         <div className="flex gap-5">
           <button
--- src/Pages/Dashboard/Home/Teacher/NoticeBoard/TeacherEventsManager.js
+++ src/Pages/Dashboard/Home/Teacher/NoticeBoard/TeacherEventsManager.js
@@ -1,34 +1,14 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import AddEventForStudent from "./AddEventForStudent";
 import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import icon from "../../../../../Assets/dashboard-icon/dashboard.png";
+import { AuthContext } from "../../../../../Contexts/AuthProvider/AuthProvider";
 
 const TeacherEventsManager = () => {
-  const [events] = useState([
-    {
-      id: 1,
-      title: "Math Quiz",
-      date: "2023-07-20",
-      description: "Quiz on chapter 3",
-      teacher: "John Doe",
-    },
-    {
-      id: 2,
-      title: "Science Experiment",
-      date: "2023-07-22",
-      description: "Hands-on experiment on chemical reactions",
-      teacher: "Jane Smith",
-    },
-    {
-      id: 3,
-      title: "Literature Discussion",
-      date: "2023-07-25",
-      description: "Analyzing a famous novel",
-      teacher: "Robert Johnson",
-    },
-    // Add more events here
-  ]);
+  const { teacher } = useContext(AuthContext);
+  const realDate = new Date().toISOString().split("T")[0];
+
   return (
     <div className="2xl:w-[79.3%] relative top-24 2xl:left-[360px]">
       <div className="text-[17px] font-semibold breadcrumbs mb-8">
@@ -40,36 +20,104 @@ const TeacherEventsManager = () => {
         </ul>
       </div>
 
-      <div className="flex gap-5">
+      <div className="flex gap-5 h-auto">
         <AddEventForStudent />
-        <div className="bg-white 2xl:px-8 2xl:py-10 w-full">
+        <div className="bg-white 2xl:px-8 2xl:py-10 w-[900px] h-[650px]">
           <h1 className="text-2xl font-bold mb-4">Manage Events</h1>
-          {events.length === 0 ? (
+          {teacher?.events?.length === 0 ? (
             <p className="text-gray-600">No upcoming events.</p>
           ) : (
-            <ul className="space-y-4 overflow-y-auto">
-              {events.map((event) => (
-                <li key={event.id} className="bg-white p-4 shadow">
-                  <div className="flex justify-between mb-4">
-                    <h1 className="text-[18px] font-bold -mb-3">
-                      {event?.title}
-                    </h1>
-                    <div className="flex gap-2">
-                      <button className="hover:scale-110">
-                        <AiOutlineEdit title="Edit" />
-                      </button>
-                      <button className="hover:text-red-500 hover:scale-110">
-                        <AiOutlineDelete title="Delete" />
-                      </button>
+            <ul className="space-y-4 overflow-y-auto h-[562px]">
+              {teacher?.events?.map((event) => {
+                const startDate = event.startDate
+                  ? new Date(event.startDate).toISOString().split("T")[0]
+                  : null;
+                const endDate = event.endDate
+                  ? new Date(event.endDate).toISOString().split("T")[0]
+                  : null;
+                let eventStatus = null;
+
+                if (realDate >= startDate && realDate <= endDate) {
+                  eventStatus = (
+                    <p className="text-white bg-green-500 py-1 px-10 text-sm text-center rounded-3xl">
+                      Running.
+                    </p>
+                  );
+                } else if (realDate > endDate) {
+                  eventStatus = (
+                    <p className="text-white bg-red-500 py-1 px-10 text-sm text-center rounded-3xl">
+                      Expired.
+                    </p>
+                  );
+                } else if (!startDate && realDate < endDate) {
+                  eventStatus = (
+                    <p className="text-white bg-yellow-500 py-1 px-10 text-sm text-center rounded-3xl">
+                      Upcoming.
+                    </p>
+                  );
+                } else if (!startDate && realDate === endDate) {
+                  eventStatus = (
+                    <p className="text-white bg-green-500 py-1 px-10 text-sm text-center rounded-3xl">
+                      Event Day.
+                    </p>
+                  );
+                } else if (!startDate && realDate > endDate) {
+                  eventStatus = (
+                    <p className="text-white bg-red-500 py-1 px-10 text-sm text-center rounded-3xl">
+                      Expired.
+                    </p>
+                  );
+                }
+
+                return (
+                  <li key={event._id} className="bg-white p-4 shadow">
+                    <div className="flex justify-between mb-4">
+                      <h1 className="text-[18px] font-bold -mb-3">
+                        {event?.eventType}: {event?.title}
+                      </h1>
+                      <div className="flex gap-2">
+                        <button className="hover:scale-110">
+                          <AiOutlineEdit title="Edit" />
+                        </button>
+                        <button className="hover:text-red-500 hover:scale-110">
+                          <AiOutlineDelete title="Delete" />
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                  <p className="text-gray-500">
-                    Date: {new Date(event.date).toLocaleDateString()}
-                  </p>
-                  <p className="text-gray-700">{event.description}</p>
-                  <p className="text-gray-500">Teacher: {event.teacher}</p>
-                </li>
-              ))}
+                    <div className="flex items-center gap-1">
+                      <p className="text-black text-[17px] font-semibold">
+                        Details:
+                      </p>
+                      <p>{event.description}</p>
+                    </div>
+
+                    {event?.eventType && event?.eventType === "Assignment" && (
+                      <div className="flex items-center gap-1">
+                        <p className="text-black font-semibold">Start Date: </p>
+                        <p>{new Date(event.startDate).toLocaleDateString()} </p>
+                      </div>
+                    )}
+                    <div className="flex items-center gap-1">
+                      {event?.eventType && event?.eventType === "Assignment" ? (
+                        <p className="text-black font-semibold">End Date:</p>
+                      ) : (
+                        <p className="text-black font-semibold">Date:</p>
+                      )}
+                      <p>{new Date(event.endDate).toLocaleDateString()} </p>
+                    </div>
+
+                    <div className="flex items-center gap-1">
+                      <p className="text-black font-semibold">Status: </p>
+                      <p>{eventStatus}</p>
+                    </div>
+
+                    <div className="flex items-center gap-1">
+                      <p className="text-black font-semibold">Teacher:</p>
+                      <p>{event.eventCreator?.name}</p>
+                    </div>
+                  </li>
+                );
+              })}
             </ul>
           )}
         </div>
